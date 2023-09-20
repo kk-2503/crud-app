31323a20458b2239e2b3d8cb977f6469f1b3de48
kk-2503 <karinakalwani.israni@volenday.com>
2023-09-20 15:46:45 +0100

    basic post and get

--- components/EmployeeForm.js
+++ components/EmployeeForm.js
@@ -1,36 +1,78 @@
+import { useState } from 'react';
+
 export function EmployeeForm() {
+	const [employee, setEmployee] = useState({
+		first_name: '',
+		last_name: '',
+		birthday: null
+	});
+
 	const handleSubmit = async e => {
 		e.preventDefault();
+
+		const firstName = e.target.elements['first-name'].value;
+		const lastName = e.target.elements['last-name'].value;
+		const birthday = e.target.elements['birthday'].value;
+
+		const formattedBirthday = formatDate(birthday);
 		const res = await fetch('/api/employees', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({
-				first_name: 'abc',
-				last_name: 'def',
-				birthday: '2007-03-25'
+				first_name: firstName,
+				last_name: lastName,
+				birthday: formattedBirthday
 			})
-		});
-		const responseData = await res.json();
+		})
+			.then(res => {
+				if (!res.ok) {
+					throw Error(res.status);
+				}
+				return res.json();
+			})
+			.then(data => {
+				console.log(data);
+			})
+			.catch(err => {
+				console.error(err);
+			});
+
+		// const responseData = await res.json();
 
 		// Handle and display the response data
-		console.log('Response data:', responseData);
+		// console.log('Response data:', responseData);
+	};
+
+	const formatDate = inputDate => {
+		const date = new Date(inputDate);
+		const year = date.getFullYear();
+		const month = String(date.getMonth() + 1).padStart(2, '0');
+		const day = String(date.getDate()).padStart(2, '0');
+		return `${year}-${month}-${day}`;
 	};
 
 	return (
-		<div className="bg-gray-300">
-			<form onSubmit={handleSubmit}>
+		<div className="w-full max-w-xs">
+			<form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 				<label htmlFor="first-name">First Name: </label>
-				<input type="text" name="first-name" />
+				<input type="text" name="first-name" className="shadow border rounded py-2 px-3 text-gray-700" />
 
 				<label htmlFor="last-name">Last Name: </label>
-				<input type="text" name="last-name" />
+				<input type="text" name="last-name" className="shadow border rounded py-2 px-3 text-gray-700" />
 
 				<label htmlFor="birthday">Birthday: </label>
-				<input type="date" id="birthday" name="birthday" />
+				<input
+					type="date"
+					id="birthday"
+					name="birthday"
+					className="shadow border rounded py-2 px-3 text-gray-700"
+				/>
 
-				<button>Add Employee</button>
+				<button className="bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded focus:outline-none focus:shadow-outline font-bold text-white">
+					Add Employee
+				</button>
 			</form>
 		</div>
 	);
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,44 @@ import React from 'react';
 
 import { EmployeeForm } from '@/components/EmployeeForm';
 
-function homePage() {
+function homePage({ employees }) {
 	return (
 		<div>
 			<EmployeeForm />
+			<div>
+				{employees.map(empl => (
+					<div key={empl.id}>
+						<h1>{empl.first_name + ' ' + empl.last_name}</h1>
+						<p>{empl.birthday.slice(0, 10)}</p>
+						<p>{empl.age}</p>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
+
+export const getServerSideProps = async context => {
+	let employees;
+	const response = await fetch('http://localhost:3000/api/employees')
+		.then(res => {
+			if (!res.ok) {
+				throw Error(res.status);
+			}
+			return res.json();
+		})
+		.then(data => {
+			employees = data;
+		})
+		.catch(err => {
+			console.error(err);
+		});
+
+	return {
+		props: {
+			employees
+		}
+	};
+};
 
 export default homePage;
